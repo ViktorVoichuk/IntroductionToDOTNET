2428bec2e1461df6c353edae8d9c3091fba602e7
Viktor <Viktor@VOKTOR-PC1>
2024-09-04 21:27:04 +0300

    changes made

---
+++ DataTypes/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static readonly string delimeter1 = "\n----------------------------------------------\n";
+        static readonly string delimeter2 = "\n==============================================\n";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# DataTypes");
+
+            #region Boolean
+            //Console.WriteLine(bool.TrueString);
+            //Console.WriteLine(Convert.ToBoolean(1));
+            //Console.WriteLine(bool.Parse("false")); 
+            #endregion
+
+            #region Char
+            ////Unicode
+            //Console.WriteLine(char.GetNumericValue(char.MinValue));
+            //Console.WriteLine(Char.MaxValue); 
+            #endregion
+
+            #region IntegralTypes
+            //Console.Write($"Переменная типа 'short' занимает {sizeof(short)} Байта памяти");
+            //Console.WriteLine($"и принимает значение в диапазоне: {short.MinValue} ... {short.MaxValue}");
+            //Console.Write($"Диапазон принимаемых значений 'ushort': {ushort.MinValue}...{ushort.MaxValue}");
+            //Console.WriteLine(delimeter1);
+
+            //Console.Write($"Переменная типа 'Int16' занимает {sizeof(Int16)} Байта памяти");
+            //Console.WriteLine($"и принимает значение в диапазоне: {Int16.MinValue} ... {Int16.MaxValue}");
+            //Console.Write($"Диапазон принимаемых значений 'UInt16': {UInt16.MinValue}...{UInt16.MaxValue}");
+            ////Convert.ToInt16();
+            //Console.WriteLine();
+            //Console.WriteLine(delimeter2);
+
+            //Console.Write($"Переменная типа 'int' занимает {sizeof(int)} Байта памяти");
+            //Console.WriteLine($"и принимает значение в диапазоне: {int.MinValue} ... {int.MaxValue}");
+            //Console.Write($"Диапазон принимаемых значений 'ushort': {uint.MinValue}...{uint.MaxValue}");
+            //Console.WriteLine(delimeter1);
+
+            //Console.Write($"Переменная типа 'Int32' занимает {sizeof(Int32)} Байта памяти");
+            //Console.WriteLine($"и принимает значение в диапазоне: {Int32.MinValue} ... {Int32.MaxValue}");
+            //Console.Write($"Диапазон принимаемых значений 'UInt32': {UInt32.MinValue}...{UInt32.MaxValue}");
+            //Console.WriteLine(delimeter2);
+
+            //Console.Write($"Переменная типа 'long' занимает {sizeof(long)} Байта памяти");
+            //Console.WriteLine($"и принимает значение в диапазоне: {long.MinValue} ... {long.MaxValue}");
+            //Console.Write($"Диапазон принимаемых значений 'ushort': {ulong.MinValue}...{ulong.MaxValue}");
+            //Console.WriteLine(delimeter1);
+
+            //Console.Write($"Переменная типа 'Int64' занимает {sizeof(Int64)} Байта памяти");
+            //Console.WriteLine($"и принимает значение в диапазоне: {Int64.MinValue} ... {Int64.MaxValue}");
+            //Console.Write($"Диапазон принимаемых значений 'UInt16': {UInt64.MinValue}...{UInt64.MaxValue}");
+            //Console.WriteLine(delimeter2);
+            #endregion
+
+
+        }
+    }
+}
--- IntroductionToDOTNET/Program.cs
+++ IntroductionToDOTNET/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define OTPUT_TO_SKREEN
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace IntroductionToDOTNET
     {
         static void Main(string[] args)
         {
+#if OTPUT_TO_SKREEN
             Console.Title = "Introduction to .NET"; //задает название консоли
             //Console.Write("Hello .NET"); //Выводит строку в консоль
             Console.WriteLine("Hello .NET"); // Вывод в консоль и переходит на новую строку
@@ -27,14 +29,45 @@ namespace IntroductionToDOTNET
             //Console.WindowHeight = 16;  //задаем размеры окна
             Console.SetWindowSize(64, 16);
 
+            //Для того чтобы свернуть часть кода его нужно поместить в регион
+            #region CodeSnippet
             ////Задаем положение окна на экране
             //Console.WindowLeft = 10;
             //Console.WindowTop = 10;
-            //Console.SetWindowPosition(10, 10);
+            //Console.SetWindowPosition(10, 10); 
+            #endregion
 
             //задаем размер буфера консоли
             Console.BufferWidth = Console.WindowWidth;
-            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferHeight = Console.WindowHeight; 
+#endif
+
+            Console.Write("Введите ваше имя: ");
+            string first_name = Console.ReadLine();//читает строку с клавиатуры до нажатия enter и возвращает прочитанную строку
+            //Console.WriteLine(first_name);
+
+            Console.Write("Введите вашу фамилию: ");
+            string last_name = Console.ReadLine();
+            //Console.WriteLine(last_name);
+
+            Console.Write("Введите ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a);
+            /*
+             * Класс Convert представляет собой набор статических методов для преобразования типов.
+             * Этот класс используется как правило в том случае когда другие преобразования не работают.
+             * */
+
+            //              Операции над строками:
+            Console.WriteLine(last_name + " " + first_name + " " + age + " years");//Конкатенация строк
+
+            //Кроме конкатенации строк С# поддерживает форматирование строк:
+            Console.WriteLine(String.Format("{0} {1} {2} years", last_name, first_name, age));
+
+            //Интерполяция строк:
+            Console.WriteLine($"{last_name} {first_name} {age} years");//Интерполяция - изменение, искажение.
+
+
         }
     }
 }
